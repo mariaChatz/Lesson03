191b9025bd30515497516719c58b44790edd2b4e
mariaChatz <DAI17129@UOM.EDU.GR>
2020-10-28 12:52:20 +0200

    add code

--- app/src/main/java/gr/uom/buttoncounterapp/MainActivity.java
+++ app/src/main/java/gr/uom/buttoncounterapp/MainActivity.java
@@ -3,12 +3,36 @@ package gr.uom.buttoncounterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button2);
+        textView=findViewById(R.id.textView);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View view) {
+                numTimesClicked++;
+                String result="\n The button clicked "+numTimesClicked+" times";
+                textView.append(result);
+
+
+            }
+        };
+        button.setOnClickListener(listener);
+
+        }
     }
 }
